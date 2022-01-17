89a0ba1d5f6f3225a0f8f270e2364f27b1c12248
Abdullah Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-01-17 14:52:34 +0300

    IdentityServer Client Credentials eklendi

---
+++ AnonFilesUpload.IdentityServer/Config.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AnonFilesUpload.IdentityServer
+{
+    public static class Config
+    {
+
+        public static IEnumerable<ApiResource> ApiResources = new ApiResource[]
+        {
+
+                new ApiResource("resource_uploadapi"){Scopes = {"api"}}
+
+                // [2] Daha sonra Resource oluşturarak buna ait scope tanımladık. 
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+         {
+
+         };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("api", "AnonFilesUpload.Api'ya full erişim iznidir."),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
+
+                // [1] İlk olarak upload-api adında bir scope tanımlaması yaptık. 
+              
+            };
+
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "VueJsClient",
+                    ClientName = "VueJS Client Credentials Client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api", IdentityServerConstants.LocalApi.ScopeName }
+                },
+            };
+    }
+}
---
+++ AnonFilesUpload.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using AnonFilesUpload.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace AnonFilesUpload.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)] 
+    // Default gelen Policy'yi ekledik. Bu sayede token olmadan buraya istek yapılamayacaktır.
+    // Token olmadan istek yapıldığında 401 hatası vereceğiz. 
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupDto dto)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = dto.UserName,
+                City = dto.City,
+                Email = dto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Hata meydana geldi");
+            }
+
+            return NoContent();
+
+        }
+    }
+}
