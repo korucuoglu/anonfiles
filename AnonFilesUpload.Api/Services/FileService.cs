261ebddb7e54bac6ae0bdffe97ce4abf1a9ce62e
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-01-26 16:01:35 +0300

    IFileService interfacesi FileService içerisinden çıkarıldı ve ayrı bir yere alındı

--- AnonFilesUpload.Api/Services/FileService.cs
+++ AnonFilesUpload.Api/Services/FileService.cs
@@ -7,22 +7,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AnonFilesUpload.Api.Services
 {
-
-    public interface IFileService
-    {
-        public Task<Response<UploadModel>> UploadAsync(IFormFile file);
-        public Task<Response<DataViewModel>> GetFilesByUserId();
-        public Task<Response<string>> GetDirectLinkByMetaId(string metaId);
-        public Task<Response<bool>> DeleteAsyncByMetaId(string metaId);
-
-    }
 
     public class FileService : IFileService
     {
---
+++ AnonFilesUpload.Api/Services/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using AnonFilesUpload.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AnonFilesUpload.Api.Services
+{
+    public interface IFileService
+    {
+        public Task<Response<UploadModel>> UploadAsync(IFormFile file);
+        public Task<Response<DataViewModel>> GetFilesByUserId();
+        public Task<Response<string>> GetDirectLinkByMetaId(string metaId);
+        public Task<Response<bool>> DeleteAsyncByMetaId(string metaId);
+
+    }
+}
