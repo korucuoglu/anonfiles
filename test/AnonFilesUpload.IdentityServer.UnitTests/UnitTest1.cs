5167c377839e9d160a738df3d90c2805281d1b62
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-02-01 14:38:54 +0300

    test projeleri eklendi

---
+++ test/AnonFilesUpload.IdentityServer.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AnonFilesUpload.IdentityServer.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
