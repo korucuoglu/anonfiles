e5e6962cce34577367e98e9a76fdf0b4f7a18c9c
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-02-03 23:38:58 +0300

    örnek test durumları eklendi

--- test/AnonFilesUpload.Api.UnitTests/Tests/Api/DataController/DataControllerTest.cs
+++ test/AnonFilesUpload.Api.UnitTests/Tests/Api/DataController/DataControllerTest.cs
@@ -1,17 +1,73 @@
 ﻿using AnonFilesUpload.Api.UnitTests.TestSetup;
-using AnonFilesUpload.Data.Entity;
+using IdentityModel.Client;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace AnonFilesUpload.Api.UnitTests.Api.DataController
 {
-    public class DataControllerTest: IClassFixture<CommonTestFixture>
+    public class DataControllerTest : IClassFixture<CommonTestFixture>
     {
-      
+        private readonly HttpClient _client;
+
+        private readonly ClientCredentialsTokenRequest clientCredentialTokenRequest = new();
+        private readonly PasswordTokenRequest passwordTokenRequest = new();
+
+        public DataControllerTest(CommonTestFixture testFixture)
+        {
+            _client = testFixture.Client;
+
+            var disco = _client.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = "http://localhost:5001",
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            }).Result;
+
+            clientCredentialTokenRequest = new ClientCredentialsTokenRequest
+            {
+                ClientId = "MVCClient",
+                ClientSecret = "secret",
+                Address = disco.TokenEndpoint
+            };
+
+            passwordTokenRequest = new PasswordTokenRequest
+            {
+                ClientId = "WebMvcClientForUser",
+                ClientSecret = "secret",
+                UserName = "test@gmail.com",
+                Password = "Test16.,",
+                Address = disco.TokenEndpoint
+            };
+
+        }
 
         [Fact]
-        public void Test1()
+        public async Task ÜyeGirişiYapılmadanFilesİstekYapıldığında401GeriyeDön()
         {
-            Assert.Equal<int>(1, 1);
+            var data = await _client.GetAsync("http://localhost:5002/api/data/myfiles");
+
+            Assert.Equal(401, (int)data.StatusCode);
+        }
+
+        [Fact]
+        public async Task ÜyeGirişiYapıldığındaDosyalaraGittiğindeGeriyeOkDön()
+        {
+            var clientCredentialToken = await _client.RequestClientCredentialsTokenAsync(clientCredentialTokenRequest);
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", clientCredentialToken.AccessToken);
+
+            Assert.Equal(200, (int)clientCredentialToken.HttpStatusCode);
+
+            var PasswordToken = await _client.RequestPasswordTokenAsync(passwordTokenRequest);
+
+            Assert.Equal(200, (int)PasswordToken.HttpStatusCode);
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", PasswordToken.AccessToken);
+
+            var response = await _client.GetAsync("http://localhost:5002/api/data/myfiles");
+
+            Assert.Equal(200, (int)response.StatusCode);
         }
     }
 }
