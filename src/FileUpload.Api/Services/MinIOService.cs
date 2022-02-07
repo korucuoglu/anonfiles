b2ba50f6e6f1fc0b09f1584891f29900aa35b4c7
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-02-07 15:01:39 +0300

    MinIO Service ile MinIO Nuget paketleri kuruldu

---
+++ src/FileUpload.Api/Services/MinIOService.cs
@@ -0,0 +1,36 @@
+﻿using FileUpload.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FileUpload.Api.Services
+{
+
+    public class MinIOService
+    {
+        public Task<Response<bool>> DeleteAsyncByMetaId(string metaId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Response<string>> GetDirectLinkByMetaId(string metaId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Response<List<MyFilesViewModel>>> GetMyFiles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Response<UploadModel>> UploadAsync(IFormFile file)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+
+
+
+}
