6a699b29aa8e9ea2e2a1835eb3cb77e66305ef39
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-02-07 18:47:54 +0300

    MinIO ile bağlantı sağlandı ve UserId üzerinden Bucket oluşturuldu

--- src/FileUpload.Api/Services/MinIOService.cs
+++ src/FileUpload.Api/Services/MinIOService.cs
@@ -1,6 +1,14 @@
-﻿using FileUpload.Shared.Models;
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Util;
+using FileUpload.Shared.Models;
+using FileUpload.Shared.Services;
 using Microsoft.AspNetCore.Http;
-using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace FileUpload.Api.Services
@@ -8,24 +16,78 @@ namespace FileUpload.Api.Services
 
     public class MinIOService
     {
-        public Task<Response<bool>> DeleteAsyncByMetaId(string metaId)
+        private readonly IConfiguration configuration;
+        private readonly ISharedIdentityService _sharedIdentityService;
+        private ILogger<MinIOService> Logger { get; set; }
+
+        public AmazonS3Client client { get; set; }
+
+        public MinIOService(IConfiguration configuration, ISharedIdentityService sharedIdentityService, ILogger<MinIOService> logger)
         {
-            throw new System.NotImplementedException();
+            this.configuration = configuration;
+            _sharedIdentityService = sharedIdentityService;
+            Logger = logger;
+
+            var config = new AmazonS3Config
+            {
+                RegionEndpoint = RegionEndpoint.GetBySystemName("us-east-1"),
+                ServiceURL = configuration["MinioAccessInfo:EndPoint"],
+                ForcePathStyle = true,
+                SignatureVersion = "2"
+            };
+
+            client = new AmazonS3Client(configuration["MinioAccessInfo:AccessKey"], configuration["MinioAccessInfo:SecretKey"], config);
+
+
         }
 
-        public Task<Response<string>> GetDirectLinkByMetaId(string metaId)
+        public async Task<string> GetBucketName(string id)
         {
-            throw new System.NotImplementedException();
+            if (!(await AmazonS3Util.DoesS3BucketExistAsync(client, _sharedIdentityService.GetUserId)))
+            {
+                var putBucketRequest = new PutBucketRequest
+                {
+                    BucketName = _sharedIdentityService.GetUserId,
+                    UseClientRegion = true
+                };
+
+                await client.PutBucketAsync(putBucketRequest);
+            }
+
+            return _sharedIdentityService.GetUserId;
+
         }
 
-        public Task<Response<List<MyFilesViewModel>>> GetMyFiles()
+        public async Task<Response<string>> UploadAsync(IFormFile file)
         {
-            throw new System.NotImplementedException();
-        }
 
-        public Task<Response<UploadModel>> UploadAsync(IFormFile file)
-        {
-            throw new System.NotImplementedException();
+            var key = string.Empty;
+            try
+            {
+                key = Guid.NewGuid().ToString();
+                var stream = file.OpenReadStream();
+                var request = new PutObjectRequest()
+                {
+                    BucketName = await GetBucketName(_sharedIdentityService.GetUserId),
+                    InputStream = stream,
+                    AutoCloseStream = true,
+                    Key = key,
+                    ContentType = file.ContentType
+                };
+                var encodedFilename = Uri.EscapeDataString(file.FileName);
+                request.Metadata.Add("original-filename", encodedFilename);
+                request.Headers.ContentDisposition = $"attachment; filename=\"{encodedFilename}\"";
+                await client.PutObjectAsync(request);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error ocurred In UploadFileAsync", e);
+                return Response<string>.Fail(e.Message, 500);
+            }
+            return Response<string>.Success(key, 200);
+
+          
+
         }
     }
 
