6f2c8536c51209fab7311c3186ae7e7752ebb993
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-02-10 13:35:26 +0300

    Api projesine AutoMapper eklendi

---
+++ src/FileUpload.Api/Mapping/GeneralMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FileUpload.Data.Entity;
+using FileUpload.Shared.Dtos.Categories;
+
+namespace FileUpload.Api.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+
+        public GeneralMapping()
+        {
+            CreateMap<Category, GetCategoriesDto>();
+        }
+    }
+}
---
+++ src/FileUpload.Shared/Dtos/Categories/GetCategoriesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUpload.Shared.Dtos.Categories
+{
+    public class GetCategoriesDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
