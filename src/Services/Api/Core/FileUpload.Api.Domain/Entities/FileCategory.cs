ccdecf9c39f042d89e0cff3769f08c7f04b23aef
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-02-26 12:24:52 +0300

    File_Category tablosundaki PK kaldırıldı ve PK olarak iki tane FK olan FileId ile CategoryId belirlendi

--- src/Services/Api/Core/FileUpload.Api.Domain/Entities/FileCategory.cs
+++ src/Services/Api/Core/FileUpload.Api.Domain/Entities/FileCategory.cs
@@ -4,7 +4,6 @@ namespace FileUpload.Domain.Entities
 {
     public class FileCategory
     {
-        public int Id { get; set; }
         public Guid FileId { get; set; }
         public File File { get; set; }
 
---
+++ src/Services/Api/Infrastructure/FileUpload.Api.Persistence/Migrations/20220226092222_FileCategoryJoinTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FileUpload.Api.Persistence.Migrations
+{
+    public partial class FileCategoryJoinTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Files_Categories",
+                table: "Files_Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Files_Categories_CategoryId",
+                table: "Files_Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Files_Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Files_Categories",
+                table: "Files_Categories",
+                columns: new[] { "CategoryId", "FileId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Files_Categories",
+                table: "Files_Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Files_Categories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Files_Categories",
+                table: "Files_Categories",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_Categories_CategoryId",
+                table: "Files_Categories",
+                column: "CategoryId");
+        }
+    }
+}
