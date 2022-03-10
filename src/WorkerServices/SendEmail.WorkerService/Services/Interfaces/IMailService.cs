1505f6e45f818b30b947f3aedc96058260e63440
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-10 15:55:51 +0300

    SendEmail WorkerService projesinde ilgili classlar DI Containera eklendi

--- src/WorkerServices/SendEmail.WorkerService/Services/Interfaces/IMailService.cs
+++ src/WorkerServices/SendEmail.WorkerService/Services/Interfaces/IMailService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SendEmail.WorkerService.Services
+namespace SendEmail.WorkerService.Services.Interfaces
 {
     public interface IMailService
     {
---
+++ src/WorkerServices/SendEmail.WorkerService/Services/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿using SendEmail.WorkerService.Services.Interfaces;
+
+namespace SendEmail.WorkerService.Services
+{
+    public class MailSettings : IMailSettings
+    {
+        public string MailAdress { get; set; }
+        public string MailPassword { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
