4b7bc1f188c08c14ffeeda5ab1ef6b80a28d36a2
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-10 20:38:32 +0300

    RabbitMQ ile mail gönderme işlemi yapıldı

---
+++ src/Shared/FileUpload.Shared/Event/RabbitMQInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUpload.Shared.Event
+{
+    public static class RabbitMQInfo
+    {
+        public const string ExchangeName = "sendmail-direct-exchange";
+        public const string RouteKey = "route-sendmail";
+        public const string QueueName = "queue-sendmail";
+    }
+}
