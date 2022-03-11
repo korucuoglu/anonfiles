82ab140b230c0e6fafda590a3419415e99681024
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-12 01:47:33 +0300

    IdentityServer projesinde ResetPassword yapısı geliştirilmeye başlandı

---
+++ src/Shared/FileUpload.Shared/Dtos/User/ConfirmEmailModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileUpload.Shared.Dtos.User
+{
+    public class ConfirmEmailModel
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+
+    }
+}
---
+++ src/Shared/FileUpload.Shared/Dtos/User/ResetPasswordModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileUpload.Shared.Dtos.User
+{
+    public class ResetPasswordModel
+    {
+
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+
+    }
+}
