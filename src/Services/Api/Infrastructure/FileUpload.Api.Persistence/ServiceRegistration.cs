1ba23024c2e8cbb6ed5a60fcfd283fe66f9f6f6d
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-19 19:30:29 +0300

    Gateway ayarlamaları yapıldı

--- src/Services/Api/Infrastructure/FileUpload.Api.Persistence/ServiceRegistration.cs
+++ src/Services/Api/Infrastructure/FileUpload.Api.Persistence/ServiceRegistration.cs
@@ -34,7 +34,7 @@ namespace FileUpload.Persistence
             services.AddAuthentication().AddJwtBearer(options =>
             {
                 options.Authority = configuration.GetSection("ServiceApiSettings").GetSection("IdentityBaseUri").Value;
-                options.Audience = "resource_upload_fullpermission";
+                options.Audience = "resource_upload";
                 options.RequireHttpsMetadata = false;
 
                 options.TokenValidationParameters = new TokenValidationParameters
