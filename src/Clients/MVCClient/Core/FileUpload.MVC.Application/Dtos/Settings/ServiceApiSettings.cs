34bc60be4620d06939dd070b1ada4988268dacd6
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-19 20:36:08 +0300

    Client Gateway üzerinden Upload servisine bağlandı

--- src/Clients/MVCClient/Core/FileUpload.MVC.Application/Dtos/Settings/ServiceApiSettings.cs
+++ src/Clients/MVCClient/Core/FileUpload.MVC.Application/Dtos/Settings/ServiceApiSettings.cs
@@ -3,6 +3,12 @@
     public class ServiceApiSettings
     {
         public string IdentityBaseUri { get; set; }
-        public string ApiBaseUri { get; set; }
+        public string GatewayBaseUri { get; set; }
+        public ServiceApi Upload { get; set; }
+    }
+
+    public class ServiceApi
+    {
+        public string Path { get; set; }
     }
 }
