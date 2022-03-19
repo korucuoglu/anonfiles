c86926f9eb11f3a92af7ec110c5546598371b772
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-03-19 18:18:08 +0300

    Nuget paketleri güncellendi

--- src/Services/Identity/FileUpload.Identity.Server/Quickstart/Account/AccountController.cs
+++ src/Services/Identity/FileUpload.Identity.Server/Quickstart/Account/AccountController.cs
@@ -9,7 +9,6 @@ using IdentityServer4.Extensions;
 using IdentityServer4.Models;
 using IdentityServer4.Services;
 using IdentityServer4.Stores;
-using IdentityServer4.Test;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
