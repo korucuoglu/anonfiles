7c1d9df578389b8aa29b50d46e9b341478ad2843
Halit Korucuoğlu <h.korucuoglu.16.06@gmail.com>
2022-04-02 21:36:48 +0300

    Bug fix

--- src/Shared/FileUpload.Shared/Wrappers/Response.cs
+++ src/Shared/FileUpload.Shared/Wrappers/Response.cs
@@ -5,6 +5,7 @@ namespace FileUpload.Shared.Wrappers
 {
     public class Response<T>
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public T Value { get; set; }
 
         [JsonIgnore]
@@ -20,22 +21,22 @@ namespace FileUpload.Shared.Wrappers
         public string Message { get; set; }
 
         // Static Factory Method
-        public static Response<T> Success(T data, int statusCode)
+        public static Response<T> Success(T data, int statusCode=0)
         {
             return new Response<T> { Value = data, StatusCode = statusCode, IsSuccessful = true };
         }
 
-        public static Response<T> Success(string message, int statusCode)
+        public static Response<T> Success(string message, int statusCode=0)
         {
             return new Response<T> { Value = default, StatusCode = statusCode, IsSuccessful = true, Message = message };
         }
 
-        public static Response<T> Success(int statusCode)
+        public static Response<T> Success(int statusCode=0)
         {
             return new Response<T> { Value = default, StatusCode = statusCode, IsSuccessful = true };
         }
 
-        public static Response<T> Fail(string error, int statusCode)
+        public static Response<T> Fail(string error, int statusCode=0)
 
         {
             return new Response<T>
@@ -46,7 +47,7 @@ namespace FileUpload.Shared.Wrappers
             };
         }
 
-        public static Response<T> Fail(List<string> errors, int statusCode)
+        public static Response<T> Fail(List<string> errors, int statusCode=0)
 
         {
             return new Response<T>
@@ -58,7 +59,7 @@ namespace FileUpload.Shared.Wrappers
         }
 
 
-        public static Response<T> Fail(T value, int statusCode)
+        public static Response<T> Fail(T value, int statusCode=0)
         {
             return new Response<T>
             {
